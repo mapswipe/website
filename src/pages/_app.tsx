ec48e199cda97b443e13c7e3f2d318e66ba2c5fc
Aditya Khatri <adityakhatri47@gmail.com>
2023-06-26 14:22:08 +0545

    Add posthog

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,28 @@
+import { useEffect } from 'react';
 import { appWithTranslation } from 'next-i18next';
 import { Lato, DM_Sans } from 'next/font/google';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import posthog from 'posthog-js';
+import { PostHogProvider } from 'posthog-js/react';
 
 import 'leaflet/dist/leaflet.css';
 import 'styles/globals.css';
 import 'styles/variables.css';
+
+// Check that PostHog is client-side (used to handle Next.js SSR)
+if (typeof window !== 'undefined' && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+        api_host: 'https://app.posthog.com',
+        persistence: 'memory',
+        // Enable debug mode in development
+        loaded: (loadedPosthog) => {
+            if (process.env.NODE_ENV === 'development') {
+                loadedPosthog.debug();
+            }
+        },
+    });
+}
 
 const lato = Lato({
     weight: ['400', '700'],
@@ -22,6 +40,17 @@ function MyApp(props: AppProps) {
         Component,
         pageProps,
     } = props;
+    const router = useRouter();
+
+    useEffect(() => {
+        // Track page views
+        const handleRouteChange = () => posthog?.capture('$pageview');
+        router.events.on('routeChangeComplete', handleRouteChange);
+
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange);
+        };
+    }, [router]);
 
     return (
         <>
@@ -38,8 +67,10 @@ function MyApp(props: AppProps) {
                 }
                 `}
             </style>
-            {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-            <Component {...pageProps} />
+            <PostHogProvider client={posthog}>
+                {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+                <Component {...pageProps} />
+            </PostHogProvider>
         </>
     );
 }
