5a1fb7f34682fafcf73349f3051a0360e41ab3ac
AdityaKhatri <adityakhatri47@gmail.com>
2026-05-08 10:55:11 +0545

    fix: add missing project type label

--- src/pages/[locale]/data/index.tsx
+++ src/pages/[locale]/data/index.tsx
@@ -42,6 +42,7 @@ import SelectInput from 'components/SelectInput';
 import {
     rankedSearchOnList,
     ProjectTypeOption,
+    ProjectType,
     ProjectStatusOption,
     getFileSizeProperties,
 } from 'utils/common';
@@ -268,57 +269,61 @@ function Data(props: Props) {
         )
     ), [projectStatusOptions]);
 
-    const projectTypeOptions: ProjectTypeOption[] = useMemo(() => ([
-        {
-            key: 'FIND',
-            label: t('type-find-title'),
-            icon: (
-                <ProjectTypeIcon type="FIND" size="small" />
-            ),
-        },
-        {
-            key: 'VALIDATE',
-            label: t('type-validate-title'),
-            icon: (
-                <ProjectTypeIcon type="VALIDATE" size="small" />
-            ),
-        },
-        {
-            key: 'COMPARE',
-            label: t('type-compare-title'),
-            icon: (
-                <ProjectTypeIcon type="COMPARE" size="small" />
-            ),
-        },
-        {
-            key: 'COMPLETENESS',
-            label: t('type-completeness-title'),
-            icon: (
-                <ProjectTypeIcon type="COMPLETENESS" size="small" />
-            ),
-        },
-        {
-            key: 'VALIDATE_IMAGE',
-            label: t('type-validate-image-title'),
-            icon: (
-                <ProjectTypeIcon type="VALIDATE_IMAGE" size="small" />
-            ),
-        },
-        {
-            key: 'STREET',
-            label: t('type-streets-view-title'),
-            icon: (
-                <ProjectTypeIcon type="STREET" size="small" />
-            ),
-        },
-        {
-            key: 'LOCATE',
-            label: t('type-locate-view-title'),
-            icon: (
-                <ProjectTypeIcon type="LOCATE" size="small" />
-            ),
-        },
-    ]), [t]);
+    const projectTypeOptions: ProjectTypeOption[] = useMemo(() => {
+        const projectTypeOptionsMapping: Record<`${ProjectType}`, ProjectTypeOption> = {
+            FIND: {
+                key: 'FIND',
+                label: t('type-find-title'),
+                icon: (
+                    <ProjectTypeIcon type="FIND" size="small" />
+                ),
+            },
+            VALIDATE: {
+                key: 'VALIDATE',
+                label: t('type-validate-title'),
+                icon: (
+                    <ProjectTypeIcon type="VALIDATE" size="small" />
+                ),
+            },
+            COMPARE: {
+                key: 'COMPARE',
+                label: t('type-compare-title'),
+                icon: (
+                    <ProjectTypeIcon type="COMPARE" size="small" />
+                ),
+            },
+            COMPLETENESS: {
+                key: 'COMPLETENESS',
+                label: t('type-completeness-title'),
+                icon: (
+                    <ProjectTypeIcon type="COMPLETENESS" size="small" />
+                ),
+            },
+            VALIDATE_IMAGE: {
+                key: 'VALIDATE_IMAGE',
+                label: t('type-validate-image-title'),
+                icon: (
+                    <ProjectTypeIcon type="VALIDATE_IMAGE" size="small" />
+                ),
+            },
+            STREET: {
+                key: 'STREET',
+                label: t('type-streets-view-title'),
+                icon: (
+                    <ProjectTypeIcon type="STREET" size="small" />
+                ),
+            },
+            LOCATE: {
+                key: 'LOCATE',
+                label: t('type-locate-title'),
+                icon: (
+                    <ProjectTypeIcon type="LOCATE" size="small" />
+                ),
+            },
+        };
+
+        return Object.values(projectTypeOptionsMapping);
+    }, [t]);
 
     const projectTypeOptionsMap = useMemo(() => (
         listToMap(
@@ -675,7 +680,7 @@ function Data(props: Props) {
                 </Card>
                 <Card
                     coverImageUrl="/img/locate_image.svg"
-                    heading={t('type-locate-view-title')}
+                    heading={t('type-locate-title')}
                     imageClassName={styles.missionImage}
                     icons={(
                         <ProjectTypeIcon
